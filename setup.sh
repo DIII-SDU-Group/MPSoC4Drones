6ffcebfaaf893e3320076ac41e7c79dae328b8c7
Frederik Falk Nyboe <ffn@mmmi.sdu.dk>
2021-10-05 15:08:02 +0200

    Added ip user files folder to setup script

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ cp .donttouch/U20U96.xpr vivado_project/
 mkdir -p vivado_project/U20U96.gen/sources_1/bd/u96v2_sbc_base/hdl/
 cp .donttouch/u96v2_sbc_base_wrapper.vhd vivado_project/U20U96.gen/sources_1/bd/u96v2_sbc_base/hdl/u96v2_sbc_base_wrapper.vhd
 cp -r .donttouch/U20U96.srcs vivado_project/
+mkdir vivado_project/U20U96.ip_user_files/
