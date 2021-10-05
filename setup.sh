738cc671220ed11295a84a24882aa9ee1a08ad40
Frederik Falk Nyboe <ffn@mmmi.sdu.dk>
2021-10-05 15:25:44 +0200

    More info in setup script

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ cp .donttouch/u96v2_sbc_base_wrapper.vhd vivado_project/U20U96.gen/sources_1/bd/
 cp -r .donttouch/U20U96.srcs vivado_project/
 mkdir vivado_project/U20U96.ip_user_files/
 
-echo "Changing ownership to current user - will prompt for sudo password"
+echo "Changing ownership of folders to current user - will prompt for sudo password"
 sudo chown -R $USER ./
 sudo chgrp -R $USER ./
 
